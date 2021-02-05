fecbbc1b386530cd5b14b709b0096332283e3557
Mikolaj <mikolajdara@gmail.com>
2021-02-05 21:03:07 +0100

    Initial commit

---
+++ Początki/script.js
@@ -0,0 +1,15 @@
+//ZADANIE 1
+
+console.log("Mikołaj Daraż")
+
+//ZADANIE 2
+
+console.log(12 + 32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+//ZADANIE 3
+
+console.log("Hello World");     //Hello World
+console.log(Hello World);       //błąd
+console.log("Hello World);      //błąd
