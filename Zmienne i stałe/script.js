8009cb7410491301f3968922fe99000dea72b653
Mikolaj <mikolajdara@gmail.com>
2021-02-05 22:06:16 +0100

    Initial commit

---
+++ Zmienne i stałe/script.js
@@ -0,0 +1,25 @@
+//ZADANIE 1 
+
+var num = 5;
+var text = "Witam serdecznie";
+var numtext = "Witam" + 2;
+var bool = true;
+var spec = null;
+console.log(num);
+console.log(text);
+console.log(numtext);
+console.log(bool)
+console.log(spec);
+
+//ZADANIE 2
+var num1 = 420;
+var num2 = 9669;
+var sum = 0;
+
+sum = num1 + num2;
+console.log(sum)
+
+//ZADANIE 3
+
+var empty
+console.log(empty)      //zmienna lub element tablicy, której nie przypisano wartości zawsze będzie miała wartość undefined, gdy zastosowane słowo kluczowe 'var'
